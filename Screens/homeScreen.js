718edaf7980f2441022cb21af225e9925f706069
Ryan Whitehouse <rwhitehouse.rw@gmail.com>
2018-06-02 23:43:37 +1200

    Refactor screens

---
+++ Screens/homeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import { createStackNavigator } from 'react-navigation';
+
+
+class HomeScreen extends React.Component {
+    render() {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Home Screen</Text>
+          <Button
+            title="Go to Details"
+            onPress={() => this.props.navigation.navigate('Details')}
+          />
+        </View>
+      );
+    }
+  }
+
+  export default HomeScreen
